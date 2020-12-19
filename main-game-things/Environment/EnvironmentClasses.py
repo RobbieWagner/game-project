b29f382700aac0e4ac39cf251fc6c964b7fe8ad1
unknown <robbie.wagner@my.simpson.edu>
2020-12-18 18:49:59 -0600

    starting next stuff. not anywhere near done -Robbie

---
+++ main-game-things/Environment/EnvironmentClasses.py
@@ -0,0 +1,9 @@
+import arcade
+
+class Adventurer(arcade.Sprite):
+    def __init__(self, image, scale):
+        super().__init__(image, scale)
+
+class Wall(arcade.Sprite):
+    def __init__(self, image, scale):
+        super().__init__(image, scale)
